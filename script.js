205ad3be567b42162fe43ebdc2b8fd07e37a6dd8
unknown <edbertwisely@gmail.com>
2024-07-03 22:10:10 +0700

    Fixing navbar ”

--- script.js
+++ script.js
@@ -8,20 +8,6 @@ function adjustHeightBasedOnWidth() {
     }
 }
 
-document.getElementById('navbar_toggle').addEventListener('click', function () {
-    document.getElementById('nav').classList.toggle('active');
-});
-
-document.getElementById('close_btn').addEventListener('click', function () {
-    document.getElementById('nav').classList.remove('active');
-});
-
-document.querySelectorAll('.nav_link').forEach(link => {
-    link.addEventListener('click', function () {
-        document.getElementById('nav').classList.remove('active');
-    });
-});
-
 document.addEventListener('scroll', () => {
     const homeContent = document.querySelector('.home_content');
     const exploreSection = document.querySelector('.explore_section');
--- server.js
+++ server.js
@@ -1,56 +1,22 @@
-// server.js
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const uri = "mongodb+srv://etozforyou:etozuntukmu123@cluster0.k8dbq0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
-app.use(express.json());
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// MongoDB Connection
-mongoose.connect(uri , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to the ETOZ_Website database");
-}).catch((error) => {
-    console.error("Error connecting to the database:", error);
+// Endpoint to send 'Hello World' message
+app.get('/message', (req, res) => {
+  res.send('Hello World');
 });
 
-const videoSchema = new mongoose.Schema({
-    title: String,
-    url: String,
-});
-
-const Video = mongoose.model('Video', videoSchema, 'Video'); // 'Video' is the name of the collection
-
-// Endpoint to Get Videos
-app.get('/videos', async (req, res) => {
-    try {
-        const videos = await Video.find();
-        res.json(videos);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// Endpoint to Get a Video by ID
-app.get('/videos/:id', async (req, res) => {
-    try {
-        const video = await Video.findById(req.params.id);
-        if (video) {
-            res.json(video);
-        } else {
-            res.status(404).json({ message: "Video not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+// Serve the 'index.html' file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`You're connected with the database`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
